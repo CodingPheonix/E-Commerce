de070053db40bb6854d3ba5624592b92c7ca7fba
Sayan Biswas <168727255+CodingPheonix@users.noreply.github.com>
2025-07-12 10:35:24 +0530

    fixed some colours

---
+++ app/Components/SVGs.js
@@ -0,0 +1,5 @@
+export const Cancel01Icon = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={30} height={30} color={"#000000"} fill={"none"} {...props}>
+    <path d="M18 6L6.00081 17.9992M17.9992 18L6 6.00085" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
+  </svg>
+);
--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,9 +1,12 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Cancel01Icon } from "@/app/Components/SVGs";
 
 export default function ProductPage({ params }) {
     const unwrapped = React.use(params)
     const id = unwrapped.id
+    const router = useRouter()
 
     const [productDetails, setProductDetails] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -29,15 +32,28 @@ export default function ProductPage({ params }) {
 
     return (
         <main className="p-6 max-w-4xl mx-auto">
-            <div className="bg-white p-6 rounded-xl shadow-md grid grid-cols-1 md:grid-cols-2 gap-6">
-                <img src={productDetails.image} alt={productDetails.title} className="h-80 object-contain" />
-                <div>
-                    <h1 className="text-3xl font-bold mb-2">{productDetails.title}</h1>
-                    <p className="text-xl text-[#64FFDA] font-semibold mb-4">${productDetails.price}</p>
-                    <p className="mb-4">{productDetails.description}</p>
-                    <p className="italic text-gray-600">Category: {productDetails.category}</p>
+            <div className="bg-white p-6 rounded-xl shadow-md flex flex-col gap-6">
+                <div onClick={() => {router.back()}} className="w-full my-1 flex justify-end">
+                    <Cancel01Icon />
+                </div>
+                <div className="flex gap-7">
+                    <img src={productDetails.image} alt={productDetails.title} className="h-80 object-contain mx-auto" />
+                    <div className="flex flex-col">
+                        <div>
+                            <h1 className="text-3xl font-bold mb-2 text-black">{productDetails.title}</h1>
+                            <p className="text-xl text-[#64FFDA] font-semibold mb-4">${productDetails.price}</p>
+                            <p className="mb-4 text-black">{productDetails.description}</p>
+                            <p className="italic text-gray-600">Category: {productDetails.category}</p>
+                        </div>
+                        <footer className="text-black my-1 flex gap-3">
+                            <button className="px-4 py-2 rounded-md bg-[#64FFDA] font-semibold text-white hover:bg-[#03e4b0]">Add to Cart</button>
+                            <button className="px-4 py-2 rounded-md bg-[#64FFDA] font-semibold text-white hover:bg-[#03e4b0]">Buy now</button>
+                        </footer>
+                    </div>
                 </div>
             </div>
         </main>
+
+
     );
 }
