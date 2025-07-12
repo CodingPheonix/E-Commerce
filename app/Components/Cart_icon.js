5e833ddba8a760177d4ac9ccc60fdec4413ea6b6
Sayan Biswas <168727255+CodingPheonix@users.noreply.github.com>
2025-07-12 12:25:11 +0530

    modified cart count dynamically

--- app/Components/Cart_icon.js
+++ app/Components/Cart_icon.js
@@ -1,11 +1,17 @@
+"use client"
 import React from 'react'
+import { useCart } from "../context/CartContext";
 
 const Cart_icon = () => {
+
+    const {cart} = useCart()
+    const number_of_items = cart.length
+
     return (
         <>
             <span className='py-2 px-4 border-2 border-black rounded-md relative'>
                 <div className='absolute w-5 h-5 text-center text-[12px] bg-red-700 rounded-full -top-2 -right-2'>
-                    5
+                    {number_of_items}
                 </div>
                 Cart
             </span>
