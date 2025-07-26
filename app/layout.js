50331efc5fe57d0a1b2bc3b0084718ba31013b40
Sayan Biswas <168727255+CodingPheonix@users.noreply.github.com>
2025-07-26 20:58:00 +0530

    added basic langgraph structure

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "./context/CartContext";
 import Navbar from "./Components/Navbar";
+import AIChatAssistant from "./Components/AI_agent";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
         <CartProvider>
           <Navbar />
           {children}
+          <AIChatAssistant />
         </CartProvider>
       </body>
     </html>
